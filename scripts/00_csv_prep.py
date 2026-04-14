ebc38f516ed7801041936e0bae588ac1081e72d8
Seamus-Finnegen <shaethomasfinnie@gmail.com>
2026-04-13 17:49:37 -0700

    Wrote 00_csv_prep.py

---
+++ scripts/00_csv_prep.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# 00_csv_prep.py
+# Extracts PSME1 and scram data from original unnormalized expression
+# csv
+#
+# Usage: python scripts/00_csv_prep.py
+# run from PSME1_KO_Expression_Analysis root 
+
+import pandas as pd
+# Load specific cols by index
+PSME1_vs_Scram = pd.read_csv(
+    "data/raw/PSME1_vs_scram_raw.csv", usecols=[0,4,5,6,7,8,9]
+    )
+# Save new csv
+PSME1_vs_Scram.to_csv("PSME1_vs_Scram.csv",index=False)
